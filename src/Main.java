58c78c61d0c95f3eb9eb78212c9c27bfc1290c4e
Sam Stabler <43603942+Stabz98@users.noreply.github.com>
2018-12-05 10:48:50 +0000

    main

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+
+	public void setup() {
+		background(0);
+
+	}
+	public void draw() {
+	
+	}
+	public void settings() {
+		size(640,480);
+	}
+	public void keyPressed() {
+
+	}
+	public void keyReleased() {
+	
+	}
+	
+}
